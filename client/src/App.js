b4202436d8d6f663a881495176e36509323a9953
marco acosta <marcucho14@hotmail.com>
2017-11-19 11:57:14 -0500

    flexing

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import Home from './components/Home'
+import Home from './components/Home';
+import Header from './components/Header';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Home />
+        </div>
+    </Router>
     );
   }
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h3>Where would you like to start today?</h3>
          <div className='button'>
                   <button className="button2">Weights</button>
-                  <button className="button3">Calis</button>
+                  <button className="button3">Calisthenics</button>
         </div>
     </div>
   );
