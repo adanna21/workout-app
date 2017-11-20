710300f4888703556a11e1a38122dcf03fc8946d
Danny Eng <dannyeng94@gmail.com>
2017-11-20 16:14:36 -0500

    fixed lift controller and model

--- controllers/liftController.js
+++ controllers/liftController.js
@@ -32,59 +32,59 @@ liftsController.show = (req, res) => {
     });
 };
 
-liftsController.create = (req, res) => {
-  Flashcard.create({
-      type: req.body.type,
-      bodyPart: req.body.bodyPart,
-      name: req.body.name,
-      reps: req.body.reps,
-      link: req.body.link,
-    })
-    .then(lifts => {
-      res.json({
-        message: 'Yup added',
-        data: {
-          lifts: lifts,
-        },
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-};
+// liftsController.create = (req, res) => {
+//   Flashcard.create({
+//       type: req.body.type,
+//       bodyPart: req.body.bodyPart,
+//       name: req.body.name,
+//       reps: req.body.reps,
+//       link: req.body.link,
+//     })
+//     .then(lifts => {
+//       res.json({
+//         message: 'Yup added',
+//         data: {
+//           lifts: lifts,
+//         },
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// };
 
-liftsController.update = (req, res) => {
-  Lifts.update({
-    type: req.body.type,
-    bodyPart: req.body.bodyPart,
-    name: req.body.name,
-    reps: req.body.reps,
-    link: req.body.link,
-  }, req.params.id).then(lift => {
-    res.json({
-      message: 'Yup added',
-      data: {
-        lifts: lifts,
-      },
-    });
-  }).catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-};
+// liftsController.update = (req, res) => {
+//   Lifts.update({
+//     type: req.body.type,
+//     bodyPart: req.body.bodyPart,
+//     name: req.body.name,
+//     reps: req.body.reps,
+//     link: req.body.link,
+//   }, req.params.id).then(lift => {
+//     res.json({
+//       message: 'Yup added',
+//       data: {
+//         lifts: lifts,
+//       },
+//     });
+//   }).catch(err => {
+//     console.log(err);
+//     res.status(500).json(err);
+//   });
+// };
 
-liftsController.delete = (req, res) => {
-  Lift.destroy(req.params.id)
-    .then(() => {
-      res.json({
-        message: 'Yup gone',
-        data: null,
-      });
-    }).catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-}
+// liftsController.delete = (req, res) => {
+//   Lift.destroy(req.params.id)
+//     .then(() => {
+//       res.json({
+//         message: 'Yup gone',
+//         data: null,
+//       });
+//     }).catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// }
 
 module.exports = liftsController;
--- models/Lift.js
+++ models/Lift.js
@@ -2,17 +2,19 @@ const db = require('../db/config');
 
 const Lift = {};
 
+Lift.findAll = () => {
+  return db.query(`
+    SELECT *
+    FROM lift
+  `);
+}
+
 Lift.findById = (id) => {
-  return db.one(`
-    INSERT INTO lift
-    (name, password_digest)
-    VALUES
-    ($1, $2)
-    RETURNING *
-  `, [
-    user.name,
-    user.password_digest
-  ]);
+  return db.oneOrNone(`
+    SELECT *
+    FROM lift
+    WHERE id = $1
+  `, [id]);
 }
 
 module.exports = Lift;
