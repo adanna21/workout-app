8a1ec98e5b975f16a9cc366d1faa3d25bb63bc41
Adanna Omatu <adaomatu@gmail.com>
2017-11-20 14:53:14 -0500

    rewrote auth

--- server.js
+++ server.js
@@ -1,20 +1,60 @@
+// import all dependencies
+const express = require('express')
+const logger = require('morgan')
+// const path = require('path')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
+
+// initialize app
+const app = express()
+
+// setup dotenv
+require('dotenv').config()
+
+// setup middleware
 app.use(logger('dev'))
-
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+  session({
+    key: process.env.SECRET_KEY,
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
+app.use(express.static('public'))
+
+// have app listen on port 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
 
+// add routes
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// comment these out for now
 const routineRouter = require('./routes/routine-routes')
 app.use('/api/routine', routineRouter)
-
 const liftRouter = require('./routes/lift-routes')
 app.use('/api/lift', liftRouter)
+const authRoutes = require('./routes/auth-routes')
+app.use('/api/auth', authRoutes)
 
+// error handlers
 app.use('*', (req, res) => {
-  res.status(404).send('Not Found')
+  res.status(400).json({
+    message: 'Not found!'
+  })
 })
 
 app.use((err, req, res, next) => {
