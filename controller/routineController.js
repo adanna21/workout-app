c9fb3623e452503b9bb4fca01e94db4612c3efe6
Adanna Omatu <adaomatu@gmail.com>
2017-11-20 15:39:12 -0500

    changed req.body for routine controller

---
+++ controller/routineController.js
@@ -0,0 +1,68 @@
+const Routine = require('../models/Routine')
+
+const routineController = {}
+
+routineController.index = (req, res, next) => {
+  Routine.findAll()
+    .then(routines => {
+      res.json({
+        message: 'ok',
+        data: {routines}
+      })
+    }).catch(next)
+}
+
+routineController.show = (req, res, next) => {
+  Routine.findById(req.params.id)
+    .then(routine => {
+      res.json({
+        message: 'ok',
+        data: {routine}
+      })
+    }).catch(next)
+}
+
+routineController.create = (req, res, next) => {
+  Routine.create({
+    name: req.body.name,
+    type: req.body.type,
+    bodyPart: req.body.bodyPart,
+    exercises1: req.body.exercises1,
+    exercises2: req.body.exercises2,
+    exercises3: req.body.exercises3,
+    exercises4: req.body.exercises4
+  }, req.user.id).then(routine => {
+    res.json({
+      message: 'Routine added successfully!',
+      data: {routine}
+    })
+  }).catch(next)
+}
+
+routineController.update = (req, res, next) => {
+  Routine.update({
+    name: req.body.name,
+    type: req.body.type,
+    bodyPart: req.body.bodyPart,
+    exercises1: req.body.exercises1,
+    exercises2: req.body.exercises2,
+    exercises3: req.body.exercises3,
+    exercises4: req.body.exercises4
+  }, req.params.id).then(routine => {
+    res.json({
+      message: 'Routine added successfully!',
+      data: {routine}
+    })
+  }).catch(next)
+}
+
+routineController.delete = (req, res, next) => {
+  Routine.destroy(req.params.id)
+    .then(() => {
+      res.json({
+        message: 'Routine deleted successfully!'
+      })
+    }).catch(next)
+}
+
+module.exports = routineController
