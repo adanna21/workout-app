d7859a8d14c2dc28ab4fd399ec3e0c705c20ee06
Danny Eng <dannyeng94@gmail.com>
2017-11-21 09:13:50 -0500

    Smoothed out a couple of minor things before trying out the React portion

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
     <div className="home">
       <h1>LIFT</h1>
       <h3>Where would you like to start today?</h3>
-         <div className='button'>
-                  <button className="button2">Weights</button>
-                  <button className="button3">Calisthenics</button>
-        </div>
+      <div className='button'>
+        <button className="button2">Weights</button>
+        <button className="button3">Calisthenics</button>
+      </div>
     </div>
   );
 };
