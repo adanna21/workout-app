66a358522bcbd9b2e463209e6636b8f4a5ed377c
marco acosta <marcucho14@hotmail.com>
2017-11-21 09:13:47 -0500

    completed login and register

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+  // constructor & state
+  constructor(props){
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    }
+    // a bind
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  // so the form can update properly
+  handleInputChange(e){
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  // rendering form
+  render(){
+    return(
+      <div>
+        <form onSubmit={(e) => this.props.handleLoginSubmit(e, this.state)}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            placeholder="Username"
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            placeholder="Password"
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="submit"
+            value="Log In!"
+          />
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default Login;
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+
+  // constructor & state
+  constructor(props){
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      email: "",
+    }
+    // a bind
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  // so the form can update properly
+  handleInputChange(e){
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  // rendering form
+  render(){
+    return(
+      <div>
+        <form onSubmit={(e) => this.props.handleRegisterSubmit(e, this.state)}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            placeholder="Username"
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            placeholder="Password"
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            placeholder="e-mail"
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="submit"
+            value="Register!"
+          />
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default Register;
