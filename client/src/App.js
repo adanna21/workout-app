59984146c4438453b636312ebe226390b8bd4b36
Danny Eng <dannyeng94@gmail.com>
2017-11-21 17:24:01 -0500

    quickfix some errors

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,11 @@ import Profile from './components/Profile'
 import Register from './components/Register'
 import './App.css'
 
-import Home from './components/Home'
-import Header from './components/Header'
+import Home from './components/Home';
+import Header from './components/Header';
 // import Footer from './components/Footer'
-import ExerciseList from './components/ExerciseList'
+import Categories from './components/Categories';
+import ExerciseList from './components/ExerciseList';
 const fetch = window.fetch
 
 class App extends Component {
@@ -32,7 +33,7 @@ class App extends Component {
     this.reset = this.reset.bind(this);
     this.getApiData = this.getApiData.bind(this);
   }
-  
+
     reset(){
     this.setState({
       apiData: null,
@@ -52,7 +53,7 @@ class App extends Component {
       });
     }).catch(err => console.log(err));
   }
-  
+
   componentDidMount () {
     fetch('/api/auth/verify', {
       credentials: 'include'
@@ -115,7 +116,7 @@ class App extends Component {
       })
     }).catch(err => console.log(err))
   }
-  
+
   getExerciseType(type){
     let data = [];
     this.state.apiData.map(lift => {
@@ -149,142 +150,37 @@ class App extends Component {
             user={this.state.user}
           />
           {this.state.apiDataLoaded ? (
-              <Route exact path="/" render={(props) =>
-                    <Home getExerciseType={this.getExerciseType} />
-                  } />
-              {/* <div className='container'> */}
-              <Route exact path='/login' render={() => (
-                this.state.auth
-                ? <Redirect to='/profile' />
-                : <Login handleLoginSubmit={this.handleLoginSubmit} />
-              )} />
-              <Route exact path='/profile' render={() => (
-                !this.state.auth
-                ? <Redirect to='/login' />
-                : <Profile user={this.state.user} />
-              )} />
-              <Route exact path='/register' render={() => (
-                this.state.auth
-                ? <Redirect to='/profile' />
-                : <Register handleRegisterSubmit={this.handleRegisterSubmit} />
-              )} />
-    // <Route exact path='/exercises' render={() => <ExerciseList auth={this.state.auth} />} />
-
-              <Route exact path="/categories" render={(props) =>
-                    <Categories getBodyType={this.getBodyType} />
-                  } />
-              <Route exact path="/routine" render={(props) =>
-                    <ExerciseList 
-                      apiData={this.state.filteredData2}
-                      auth={this.state.auth}/>
-                  } />
+              <div>
+                <Route exact path="/" render={(props) =>
+                      <Home getExerciseType={this.getExerciseType} />
+                    } />
+                <Route exact path='/login' render={() => (
+                  this.state.auth
+                  ? <Redirect to='/profile' />
+                  : <Login handleLoginSubmit={this.handleLoginSubmit} />
+                )} />
+                <Route exact path='/profile' render={() => (
+                  !this.state.auth
+                  ? <Redirect to='/login' />
+                  : <Profile user={this.state.user} />
+                )} />
+                <Route exact path='/register' render={() => (
+                  this.state.auth
+                  ? <Redirect to='/profile' />
+                  : <Register handleRegisterSubmit={this.handleRegisterSubmit} />
+                )} />
+                <Route exact path="/categories" render={(props) =>
+                      <Categories getBodyType={this.getBodyType} />
+                    } />
+                <Route exact path="/routine" render={(props) =>
+                      <ExerciseList
+                        apiData={this.state.filteredData2}
+                        auth={this.state.auth}/>
+                    } />
+              </div>
            ) : (
             <p>Loading...</p>
           )}
-           
-          {/* </div> */}
-          {/* <Footer /> */}
-
-
-// import React, { Component } from 'react';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import './App.css';
-
-// import Header from './components/Header';
-// import Home from './components/Home';
-// import Categories from './components/Categories';
-// // import Login from './components/Login';
-// // import Register from './components/Register';
-// // import Profile from './components/Profile';
-// import ExerciseList from './components/ExerciseList';
-
-// class App extends Component {
-
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       apiData: null,
-//       apiDataLoaded: false,
-//       filteredData1: null,
-//       filteredData2: null,
-//     }
-//     this.getBodyType = this.getBodyType.bind(this);
-//     this.getExerciseType = this.getExerciseType.bind(this);
-//     this.reset = this.reset.bind(this);
-//     this.getApiData = this.getApiData.bind(this);
-//   }
-
-//   reset(){
-//     this.setState({
-//       apiData: null,
-//       apiDataLoaded: false,
-//       filteredData1: null,
-//       filteredData2: null,
-//     })
-//   }
-
-//   getApiData(){
-//     fetch('/api/lift')
-//     .then(res => res.json())
-//     .then(res => {
-//       this.setState({
-//         apiData: res.data.lifts,
-//         apiDataLoaded: true,
-//       });
-//     }).catch(err => console.log(err));
-//   }
-
-//    componentDidMount(){
-//     this.getApiData();
-//   }
-
-//   getExerciseType(type){
-//     let data = [];
-//     this.state.apiData.map(lift => {
-//       if (lift.type === type){
-//         data.push(lift)
-//       }
-//     })
-//     this.setState({
-//       filteredData1: data,
-//     })
-//   }
-
-//   getBodyType(bodyType){
-//     let data = [];
-//     this.state.filteredData1.map(lift => {
-//       if (lift.bodypart === bodyType){
-//         data.push(lift)
-//       }
-//     })
-//     this.setState({
-//       filteredData2: data,
-//     })
-//   }
-
-//   render(){
-//     return (
-//       <Router>
-//         <div className="App">
-//           <Header reset={this.reset}/>
-//           {this.state.apiDataLoaded ? (
-//             <div>
-//               <Route exact path="/" render={(props) =>
-//                 <Home getExerciseType={this.getExerciseType} />
-//               } />
-//               <Route exact path="/categories" render={(props) =>
-//                 <Categories getBodyType={this.getBodyType} />
-//               } />
-//               {/*<Route exact path="/login" component={Login} />
-//               <Route exact path="/register" component={Register} />
-//               <Route exact path="/profile" component={Profile} />*/}
-//               <Route exact path="/routine" render={(props) =>
-//                 <ExerciseList apiData={this.state.filteredData2} />
-//               } />
-//             </div>
-//           ) : (
-//             <p>Loading...</p>
-//           )}
 
         </div>
       </Router>
