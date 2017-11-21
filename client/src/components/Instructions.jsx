612cfaa947ab58e596ad48f2a4adfc0402e4f44e
Matthew Sucharski <a7x1013@aol.com>
2017-11-21 13:55:38 -0500

    iframes added

---
+++ client/src/components/Instructions.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom'
+
+const Instructions = {props} => {
+  return (
+    <div className="instructions">{this.state.instructions}</div>
+      <h2>How To</h2>
+        <iframe src="http://www.youtube.com/embed/" + this.props.instructions
+      }
+      frameBorder = "0"
+      allowFullScreen > < /iframe> </div>
+  )
+}
+export default Instructions;
