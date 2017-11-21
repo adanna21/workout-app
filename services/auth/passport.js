3f5e39515b5f2819137bcddbba3c059fc107e90e
Adanna Omatu <adaomatu@gmail.com>
2017-11-21 15:54:57 -0500

    fixed user undefined error in passport

--- services/auth/passport.js
+++ services/auth/passport.js
@@ -1,14 +1,13 @@
 const passport = require('passport');
-const Player = require('../../models/User');
+const User = require('../../models/User');
 
 module.exports = () => {
-
   passport.serializeUser((user, done) => {
     done(null, user.username);
   });
 
   passport.deserializeUser((username, done) => {
-    User.findByUsername(username)
+    User.findByUserName(username)
       .then(user => {
         done(null, user);
       }).catch(err => {
