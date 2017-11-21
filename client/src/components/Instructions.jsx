5eb51ce29cb37b03b1ea961f97696c01898a9bcb
Danny Eng <31488188+danny-eng@users.noreply.github.com>
2017-11-21 14:01:58 -0500

    syntax changes

--- client/src/components/Instructions.jsx
+++ client/src/components/Instructions.jsx
@@ -4,12 +4,11 @@ import { Link } from 'react-router-dom'
 
 const Instructions = {props} => {
   return (
-    <div className="instructions">
+    <div className="instructions">{this.state.instructions}
       <h2>How To</h2>
-        <iframe src="http://www.youtube.com/embed/" + {this.props.instructions}
-      frameBorder = "0"
-      allowFullScreen > < /iframe> 
+        <iframe src={`http://www.youtube.com/embed/${this.props.instructions}`}></iframe> 
     </div>
   )
 }
+
 export default Instructions;
