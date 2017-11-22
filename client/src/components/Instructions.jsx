8c0dabf5ad9bcebeb34de19a74102c187a968a3c
Adanna Omatu <adaomatu@gmail.com>
2017-11-22 14:29:11 -0500

    added title to instructions component

--- client/src/components/Instructions.jsx
+++ client/src/components/Instructions.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-
 import Iframe from 'react-iframe'
 
 function Instructions (props) {
   return (
     <div className='instructions'>
-      <h2>How To</h2>
+      <h2>How To {props.selectedExercise.name}</h2>
       <Iframe width='420' height='345' url={props.selectedExercise.link} />
     </div>
   )
