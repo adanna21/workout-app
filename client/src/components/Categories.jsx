b9945674a82ebceb9f65810054ca06683ecc8d96
Adanna Omatu <adaomatu@gmail.com>
2017-11-22 10:48:47 -0500

    created class in exercise

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Categories(props){
-  return(
-    <div className="button-list">
-      <div className="chest-button" onClick={() => props.getBodyType("chest")}>
-        <Link to="/routine">Chest</Link>
+function Categories (props) {
+  return (
+    <div className='button-list'>
+      <div className='chest-button' onClick={() => props.getBodyType('chest')}>
+        <Link to='/routine'>Chest</Link>
       </div>
-      <div className="legs-button" onClick={() => props.getBodyType("legs")}>
-        <Link to="/routine">Legs</Link>
+      <div className='legs-button' onClick={() => props.getBodyType('legs')}>
+        <Link to='/routine'>Legs</Link>
       </div>
-      <div className="back-button" onClick={() => props.getBodyType("back")}>
-        <Link to="/routine">Back</Link>
+      <div className='back-button' onClick={() => props.getBodyType('back')}>
+        <Link to='/routine'>Back</Link>
       </div>
-      <div className="delts-button" onClick={() => props.getBodyType("delts")}>
-        <Link to="/routine">Delts</Link>
+      <div className='delts-button' onClick={() => props.getBodyType('delts')}>
+        <Link to='/routine'>Delts</Link>
       </div>
     </div>
   )
-
 }
 
-export default Categories;
+export default Categories
--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function Exercise(props){
-  return(
-    <div className="exercise-card">
-      <p>{props.exercise.name}</p>
-    </div>
-  )
+class Exercise extends Component {
+  construcor (props) {
+    super(props)
+    // this.state = {
+    //
+    // }
+  }
+  render () {
+    return (
+      <div className='exercise-card'>
+        <p>{this.props.exercise.name}</p>
+      </div>
+    )
+  }
 }
 
-export default Exercise;
+export default Exercise
