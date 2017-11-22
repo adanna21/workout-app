f8bed7ca019a7d6f44a7f61d684db1d7ce51507f
Adanna Omatu <adaomatu@gmail.com>
2017-11-22 11:42:56 -0500

    installed 'react-iframe' to render iframes

--- client/src/components/Instructions.jsx
+++ client/src/components/Instructions.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom'
+import Iframe from 'react-iframe'
 
 function Instructions (props) {
-  console.log(props.apiData);
+  console.log(props.apiData)
   return (
     <div className='instructions'>
       <h2>How To</h2>
-      <iframe src={props.apiData.link} />
+      <Iframe width='420' height='345' url={props.apiData.link} />
     </div>
   )
 }
