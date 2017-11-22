4fd5d6190028115d34fab1c67cbff23850c5da98
marco acosta <marcucho14@hotmail.com>
2017-11-22 10:46:22 -0500

    styling home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 function Home(props){
   return (
     <div className="home">
-      <h1>LIFT</h1>
-      <h3>Where would you like to start today?</h3>
+      <h1 id="name">iLIFT</h1>
+      <h3 id="title">Where would you like to start today?</h3>
       <div className='button'>
-        <Link className='button2' to="/categories" onClick={() => props.getExerciseType("weight")}>Weights</Link>
-        <Link className='button3' to="/categories" onClick={() => props.getExerciseType("calisthenics")}>Calisthenics</Link>
+        <Link className='button2' to="/categories" style={{ textDecoration: 'none'}} onClick={() => props.getExerciseType("weight")}>Weights</Link>
+        <Link className='button3' to="/categories" style={{ textDecoration: 'none' }} onClick={() => props.getExerciseType("calisthenics")}>Calisthenics</Link>
       </div>
     </div>
   );
