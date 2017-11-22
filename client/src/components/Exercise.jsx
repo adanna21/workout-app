61df02908333bb28a0f7b19cbe903561ee7da5a7
Adanna Omatu <adaomatu@gmail.com>
2017-11-22 14:32:06 -0500

    remove class from exercise component

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,37 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import Instructions from './Instructions'
 
-class Exercise extends Component {
-  constructor (props) {
-    super(props)
-    // this.state = {
-    //   instructionsClicked: false
-    // }
-    // this.changeInsClicked = this.changeInsClicked.bind(this)
-  }
+function Exercise (props) {
+  return (
+    <div className='exercise-card'>
 
-  // changeInsClicked () {
-  //   console.log(this.props.exercise.link);
-  //   this.setState({
-  //     instructionsClicked: true
-  //   })
-  // }
+      <h3>{this.props.exercise.name}</h3>
+      <p>{this.props.exercise.sets}</p>
+      <p>{this.props.exercise.reps}</p>
+      <button onClick={() => this.props.selectExerciseById(this.props.exercise.id)}>
+        <Link to={`/instructions/${this.props.exercise.id}`}> Instructions </Link>
+      </button>
 
-  render () {
-    return (
-      <div className='exercise-card'>
-
-        <h3>{this.props.exercise.name}</h3>
-        <p>{this.props.exercise.sets}</p>
-        <p>{this.props.exercise.reps}</p>
-        <button onClick={() => this.props.selectExerciseById(this.props.exercise.id)}>
-          <Link to={`/instructions/${this.props.exercise.id}`}> Instructions </Link>
-        </button>
-
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Exercise
