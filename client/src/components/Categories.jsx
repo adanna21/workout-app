f16abd8b6bbb27e2ff1f4edb9fc5fe63d5356e3a
marco acosta <marcucho14@hotmail.com>
2017-11-24 11:20:30 -0500

    more styling

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -5,16 +5,16 @@ function Categories(props){
   return(
     <div className="button-list">
       <div className="chest-button" onClick={() => props.getBodyType("chest")}>
-        <Link to="/routine">Chest</Link>
+        <Link to="/routine"><h1 id="text">Chest</h1></Link>
       </div>
       <div className="legs-button" onClick={() => props.getBodyType("legs")}>
-        <Link to="/routine">Legs</Link>
+        <Link to="/routine"><h1 id="text">Legs</h1></Link>
       </div>
       <div className="back-button" onClick={() => props.getBodyType("back")}>
-        <Link to="/routine">Back</Link>
+        <Link to="/routine"><h1 id="text">Back</h1></Link>
       </div>
       <div className="delts-button" onClick={() => props.getBodyType("delts")}>
-        <Link to="/routine">Delts</Link>
+        <Link to="/routine"><h1 id="text">Delts</h1></Link>
       </div>
     </div>
   )
