fcd570069b77aead4206e60e5f63e17018237fec
Adanna Omatu <adaomatu@gmail.com>
2017-11-27 09:53:09 -0500

    logout works now

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-function Header(props){
-  console.log("+++ header +++")
+function Header (props) {
+  console.log('+++ header +++')
   console.log(props)
   return (
     <header>
-       <div className="logo"></div>
+      <div className='logo' />
       {props.auth ? (
         <nav>
           <ul>
-            <li><Link to='/'style={{ textDecoration: 'none'}} >Home</Link></li>
-            <li><Link to='/workouts' style={{ textDecoration: 'none'}}>Workouts</Link></li>
-            <li><Link to='/routine' style={{ textDecoration: 'none'}}>Add Routine</Link></li>
-            <li><Link to='/login' style={{ textDecoration: 'none'}} onClick={() => props.logout}>Logout</Link></li>
+            <li><Link to='/'style={{textDecoration: 'none'}} >Home</Link></li>
+            <li><Link to='/workouts' style={{textDecoration: 'none'}}>Workouts</Link></li>
+            <li><Link to='/routine' style={{textDecoration: 'none'}}>Add Routine</Link></li>
+            <li><Link to='/login' style={{textDecoration: 'none'}} onClick={() => props.logout()}>Logout</Link></li>
           </ul>
         </nav>
       ) : (
         <nav>
           <ul>
-            <li><Link to='/' style={{ textDecoration: 'none'}}>Home</Link></li>
-            <li><Link to='/login' style={{ textDecoration: 'none'}}>Login</Link></li>
-            <li><Link to='/register' style={{ textDecoration: 'none'}}>Register</Link></li>
+            <li><Link to='/' style={{textDecoration: 'none'}}>Home</Link></li>
+            <li><Link to='/login' style={{textDecoration: 'none'}}>Login</Link></li>
+            <li><Link to='/register' style={{textDecoration: 'none'}}>Register</Link></li>
           </ul>
         </nav>
       )}
