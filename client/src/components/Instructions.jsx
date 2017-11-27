62892b49a8476ea676293755025fa40530f64bcc
Danny Eng <dannyeng94@gmail.com>
2017-11-27 10:46:07 -0500

    added a back button on instructions

--- client/src/components/Instructions.jsx
+++ client/src/components/Instructions.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Iframe from 'react-iframe'
+import { Link } from 'react-router-dom'
+
 
 function Instructions (props) {
   return (
     <div className='instructions'>
       <h2>How To {props.selectedExercise.name}</h2>
       <Iframe width='420' height='345' url={props.selectedExercise.link} />
+      <Link to='/routine'>Back</Link>
     </div>
   )
 }
