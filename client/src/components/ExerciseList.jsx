2c0332195f2c950f30697eda1fa941d187882d63
Danny Eng <dannyeng94@gmail.com>
2017-11-27 12:38:59 -0500

    save works again

--- client/src/components/ExerciseList.jsx
+++ client/src/components/ExerciseList.jsx
@@ -1,16 +1,76 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
+import React, { Component } from 'react'
+// import { withRouter } from 'react-router-dom'
 import Exercise from './Exercise'
 
-function ExerciseList (props) {
-  console.log(props)
-  return (
-    <div className='exercise-list'>
-      {props.apiData.map(exercise => {
-        return <Exercise key={exercise.id} exercise={exercise} selectExerciseById={props.selectExerciseById} />
-      })}
-    </div>
-  )
+class ExerciseList extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      list: null
+    }
+  }
+
+  componentDidMount(){
+
+    // stack overflow's shuffle an array
+    let array = this.props.apiData
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+
+    }
+
+    this.setState({
+      list: array.slice(0,4)
+    })
+
+  }
+
+  saveRoutine(){
+    fetch('/api/routine/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: "New Routine",
+        type: this.state.list[0].type,
+        bodypart: this.state.list[0].bodypart,
+        user_id: this.props.user.id,
+        exercises1: this.state.list[0].id,
+        exercises2: this.state.list[1].id,
+        exercises3: this.state.list[2].id,
+        exercises4: this.state.list[3].id
+      })
+    }).catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div className='exercise-list'>
+        {this.state.list ? (
+          this.state.list.map(exercise => {
+          return <Exercise key={exercise.id} exercise={exercise} selectExerciseById={this.props.selectExerciseById} />
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+        <button onClick={() => this.saveRoutine()}>Save</button>
+      </div>
+    )
+  }
 }
 
-export default withRouter(ExerciseList)
+// export default withRouter(ExerciseList)
+export default ExerciseList
