0d0bfb1e4aa8e5dd8bb0f9d9e52c576d60562374
Adanna Omatu <adaomatu@gmail.com>
2017-11-28 15:44:56 -0500

    added style change to inst

--- client/src/components/Instructions.jsx
+++ client/src/components/Instructions.jsx
@@ -9,6 +9,7 @@ class Instructions extends Component {
   render () {
     return (
       <div className='instructions'>
+<<<<<<< HEAD
         <h2>How To {this.props.selectedExercise.name}</h2>
         <Iframe width='50%' height='50%' url={this.props.selectedExercise.link} />
         <div className='link-wrapper'>
@@ -16,13 +17,21 @@ class Instructions extends Component {
             <Link to='/profile' className='routine-back'>Back</Link> :
           this.props.source === "exercise" ?
             <Link to='/routine' className='routine-back'>Back</Link> :
+=======
+        <h2>How To Do {this.props.selectedExercise.name}</h2>
+        <Iframe width='60%' height='60%' allowFullScreen url={this.props.selectedExercise.link} />
+        <div className='link-wrapper'>
+          {this.props.source === 'profile' ?
+            <Link to='/profile' className='instructions-btn'>Back</Link> :
+          this.props.source === 'exercise' ?
+            <Link to='/routine' className='instructions-btn'>Back</Link> :
+>>>>>>> added style change to inst
             <Link to='/'>Error</Link>
           }
         </div>
       </div>
     )
   }
-
 }
 
 export default Instructions
