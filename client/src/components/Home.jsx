e9f84283cb2d47ad57e2c1932c9e2813b91aaa96
Matthew Sucharski <a7x1013@aol.com>
2017-11-28 16:50:55 -0500

    changed home page tag line

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -14,7 +14,8 @@ class Home extends Component {
     return (
       <div className='home'>
         {/* <h1 id="name">iLIFT</h1> */}
-        <h3 id='title'>WHERE WOULD YOU LIKE TO START TODAY?</h3>
+        <h3 id='title'>START WORKING OUT TODAY, ILIFT!</h3>
+        <h4>* SELECT WEIGHTS OR YOUR OWN BODYWEIGHT *</h4>
         <div className='button'>
           <Link className='button2' to='/categories' onClick={() => this.props.getExerciseType('weight')}>Weights</Link>
           <Link className='button3' to='/categories' onClick={() => this.props.getExerciseType('calisthenics')}>Calisthenics</Link>
